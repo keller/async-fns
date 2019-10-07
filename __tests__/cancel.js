5fb5a345bc53a9a5e52be70475d593efff6c31cf
Keller <keller.davis@gmail.com>
2019-10-06 18:44:29 -0700

    more tests for cancel

--- __tests__/cancel.js
+++ __tests__/cancel.js
@@ -11,12 +11,41 @@ describe("cancel", () => {
   it("should throw when canceled", () => {
     const ERROR = { error: "error!" };
     const source = cancel.source();
-    const run = cancel.wrap(() => waitAndEcho(45), source.token);
+    const run = cancel.wrap(() => waitAndEcho(10), source.token);
 
     setTimeout(() => {
       source.cancel(ERROR);
     }, 100);
 
     return run().catch(e => expect(e).toEqual(ERROR));
+  });
+
+  it("should cancel multiple async fns", () => {
+    const ERROR = { error: "error!" };
+    const source = cancel.source();
+    const run = cancel.wrap(() => waitAndEcho(10), source.token);
+    const run2 = cancel.wrap(() => waitAndEcho(42), source.token);
+
+    setTimeout(() => {
+      source.cancel(ERROR);
+    }, 100);
+
+    return run().catch(e => expect(e).toEqual(ERROR));
+    return run2().catch(e => expect(e).toEqual(ERROR));
+  });
+
+  it("shouldn't cancel all fns", () => {
+    const ERROR = { error: "error!" };
+    const source = cancel.source();
+    const source2 = cancel.source();
+    const run = cancel.wrap(() => waitAndEcho(10), source.token);
+    const run2 = cancel.wrap(() => waitAndEcho(42), source2.token);
+
+    setTimeout(() => {
+      source.cancel(ERROR);
+    }, 100);
+
+    return run().catch(e => expect(e).toEqual(ERROR));
+    return run2().resolves.toEqual(42);
   });
 });
