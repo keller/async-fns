b0c04562c85e64cc2183b87f02d54a30609d57d5
Keller <keller.davis@gmail.com>
2019-10-08 23:00:02 -0700

    inital run an channels

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import sequence from "../sequence/src";
 import parallel from "../parallel/src";
 import cancel from "../cancel/src";
 import emitter from "../emitter/src";
+import run, { channel } from "../run/src";
 
-export { sequence, parallel, cancel, emitter };
+export { sequence, parallel, cancel, emitter, run };
 
-export default { sequence, parallel, cancel, emitter };
+export default { sequence, parallel, cancel, emitter, channel };
