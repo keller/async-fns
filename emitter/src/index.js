c8a1629f2acdbde08ba9312027035abf6eb1bc8f
Keller <keller.davis@gmail.com>
2019-10-23 08:05:47 -0700

    allow custom or reused listeners to emitter

--- emitter/src/index.js
+++ emitter/src/index.js
@@ -1,13 +1,13 @@
-export default function emitter() {
-  const listeners = {};
+export default function emitter(listeners) {
+  listeners = listeners || {};
   return {
     subscribe(name, listener) {
       (listeners[name] || (listeners[name] = [])).push(listener);
       return () => listeners[name].splice(listeners[name].indexOf(listener), 1);
     },
     emit(name, msg) {
-      [...listeners[name] || []].map(cb => cb(msg));
-      [...listeners["*"] || []].map(cb => cb(name, msg));
+      [...(listeners[name] || [])].map(cb => cb(msg));
+      [...(listeners["*"] || [])].map(cb => cb(name, msg));
     }
   };
 }
