f98c41ab9ec0927aeaea685141da75affa78d0e7
Keller Davis <keller.davis@biblegateway.com>
2021-12-06 11:22:44 -0800

    put cached vars in right place

--- memo/src/index.js
+++ memo/src/index.js
@@ -1,8 +1,8 @@
 function shallowCompareArray(a, b) {
   return a.length === b.length && a.every((e, i) => e === b[i]);
 }
-let cached, prevArgs;
 export default function memo(fn) {
+  let cached, prevArgs;
   return function(...args) {
     if (!prevArgs || !shallowCompareArray(args, prevArgs)) {
       cached = fn(...args);
